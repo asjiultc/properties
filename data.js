5ab188c1acd03dc6a50cc67bd0d65c520f754785
asjiultc <135724151+asjiultc@users.noreply.github.com>
2026-08-05 14:31:22 +0200

    Comment out property image path
    Comment out the path for property-003/04.jpg.

--- data.js
+++ data.js
@@ -226,7 +226,7 @@ Kafshet shtepiake nuk lejohen(qen, mace)
         "assets/properties/property-003/02.jpg",
 
         "assets/properties/property-003/03.jpg",
-        "assets/properties/property-003/04.jpg",
+        //"assets/properties/property-003/04.jpg",
         "assets/properties/property-003/05.jpg",
         "assets/properties/property-003/06.jpg",
         "assets/properties/property-003/07.jpg",
